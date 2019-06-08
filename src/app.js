e4b27bfc634596cc109bbeea01c60147ddd104d2
Krivax <krivahz@gmail.com>
2019-06-08 09:28:58 -0300

    desafio 045

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@
   paths[""] = login;
   paths["#/login"] = login;
   paths["#/signup"] = signup;
+  paths["#/game"] = game;
+  paths["#/404"] = error404;
 
-  paths[hash]();
+  if (paths[hash] === undefined) {
+    paths["#/404"]();
+  } else {
+    paths[hash]();
+  }
 })();
